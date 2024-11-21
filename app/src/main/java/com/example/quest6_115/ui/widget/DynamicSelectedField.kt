722e8234f4368baa485e00cde688a06ef50f612d
Khaiqol <Khaiqol@gmail.com>
2024-11-21 16:50:10 +0700

    Code yang Anda berikan mendefinisikan sebuah Composables

--- app/src/main/java/com/example/quest6_115/ui/widget/DynamicSelectedField.kt
+++ app/src/main/java/com/example/quest6_115/ui/widget/DynamicSelectedField.kt
@@ -1,2 +1,17 @@
 package com.example.quest6_115.ui.widget
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DynamicSelectedTextField(
+    selectedValue: String,
+    options: List<String>,
+    label: String,
+    onValueChangeEvent: (String) -> Unit,
+    modifier: Modifier = Modifier
+) {
+
+}
