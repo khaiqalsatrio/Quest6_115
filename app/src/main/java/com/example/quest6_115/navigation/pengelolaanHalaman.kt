5a591bb197b7cde2e3cd8ceac31b1a58a070bd4e
Khaiqol <Khaiqol@gmail.com>
2024-11-21 16:27:30 +0700

    membuat fun halaman sebagai controler dan enum class halaman

--- app/src/main/java/com/example/quest6_115/navigation/pengelolaanHalaman.kt
+++ app/src/main/java/com/example/quest6_115/navigation/pengelolaanHalaman.kt
@@ -1,4 +1,37 @@
 package com.example.quest6_115.navigation
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 
+enum class Halaman {
+    MahasiswaForm,
+    RencanaStudy,
+    Splash,
+    Tampil,
+}
+
+@Composable
+fun PengelolaanHalaman(
+    modifier: Modifier = Modifier,
+    navHost: NavHostController = rememberNavController()
+) {
+    // State untuk menyimpan data mahasiswa
+    val mahasiswa = remember {
+        mutableStateOf(
+            Mahasiswa(
+                Nama = "",
+                Nim = "",
+                Email = "",
+                Matakuliah = "",
+                Kelas = ""
+            )
+        )
+    }
 
