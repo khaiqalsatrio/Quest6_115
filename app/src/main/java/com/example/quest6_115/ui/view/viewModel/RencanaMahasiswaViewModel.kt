c09ad8f9fc860fcd409e5e5debb09b4aebc4b3d9
Khaiqol <Khaiqol@gmail.com>
2024-11-21 16:46:55 +0700

    membuat fun di class Rencana mahasiswa view model

--- app/src/main/java/com/example/quest6_115/ui/view/viewModel/RencanaMahasiswaViewModel.kt
+++ app/src/main/java/com/example/quest6_115/ui/view/viewModel/RencanaMahasiswaViewModel.kt
@@ -1,4 +1,13 @@
 package com.example.quest6_115.ui.view.viewModel
 
-class RencanaMahasiswaViewModel {
+import androidx.lifecycle.ViewModel
+import com.example.quest6_115.model.RencanaStudi
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class RencanaMahasiswaViewModel : ViewModel() {
+
+    private val _krsState = MutableStateFlow(RencanaStudi())
+    val krsStateUi: StateFlow<RencanaStudi> = _krsState.asStateFlow()
 }
