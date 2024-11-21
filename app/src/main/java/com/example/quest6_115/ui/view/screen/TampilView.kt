43dfe935498e6e02876ffcf0847fec471c6386ad
Khaiqol <Khaiqol@gmail.com>
2024-11-21 16:40:02 +0700

    membuat fun di halaman tampil view untuk menampilkan data mahasiswa yg sudah di simpan

--- app/src/main/java/com/example/quest6_115/ui/view/screen/TampilView.kt
+++ app/src/main/java/com/example/quest6_115/ui/view/screen/TampilView.kt
@@ -1,2 +1,54 @@
 package com.example.quest6_115.ui.view.screen
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.quest6_115.model.Mahasiswa
+
+@Composable
+fun TampilView(
+    uiState: Mahasiswa,
+    onBackButtonClicked: () -> Unit = {},
+    onSubmitButtonClicked: () -> Unit = {}
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = colorResource(id = R.color.primary)),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Spacer(modifier = Modifier.height(16.dp))
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.padding(horizontal = 16.dp)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.umy2),
+                contentDescription = "Logo Universitas",
+                modifier = Modifier.size(45.dp)
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            Column {
+                Text(
+                    text = "Universitas Muhamadiyah Yogyakarta",
+                    color = Color.White,
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold
+                )
+                Text(
+                    text = "Unggul Dan Islami",
+                    color = Color.White,
+                    fontWeight = FontWeight.Light
+                )
+            }
+        }
